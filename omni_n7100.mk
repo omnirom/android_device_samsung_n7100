06045a733072de5c6e7c2de2cc6f06537096dc31
tilaksidduram <tilaksidduram@gmail.com>
2013-11-06 23:17:09 +0530

    [N7100] - Fixes for Device bringup -defconfig -Sound fix -omni gsm makefile Change-Id: I2dc95e1f43eaaa4b93ca6ea30068d6d2515c8e4b

--- omni_n7100.mk
+++ omni_n7100.mk
@@ -13,6 +13,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Inherit Omni GSM telephony parts
+$(call inherit-product, vendor/omni/config/gsm.mk)
+
 # Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
@@ -29,3 +32,4 @@ PRODUCT_DEVICE := n7100
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := Samsung
 PRODUCT_MODEL := GT-N7100
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=t03gxx TARGET_DEVICE=t03g BUILD_FINGERPRINT="samsung/t03gxx/t03g:4.1.1/JRO03C/N7100XXALJ3:user/release-keys" PRIVATE_BUILD_DESC="t03gxx-user 4.1.1 JRO03C N7100XXALJ3 release-keys"
